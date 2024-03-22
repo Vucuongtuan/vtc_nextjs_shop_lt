2edc232d74d65570f75a0ee3fc8d85200c0f4901
Vucuongtuan <vucuongtuan00@gmail.com>
2024-03-22 14:24:56 +0700

    new

--- src/app/product/components/ListProductPage.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ListProductPage = () => {
-  return <div>ListProductPage</div>;
-};
-
-export default ListProductPage;
--- src/service/user/index.ts
+++
@@ -1,9 +0,0 @@
-import http from "@/utils/http";
-
-export const SigninAccount = async (
-  username: string,
-  password: string | number
-) => {
-  const res = await http.post("/account/signin", { username, password });
-  return res.data;
-};
