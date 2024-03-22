e21eae6fd529ac29aa50f77c8304b24da2447147
Vucuongtuan <vucuongtuan00@gmail.com>
2024-03-22 14:29:12 +0700

    a

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"MSWnuAaQ6xWdbdrWkxkysjr64K4FLiN7NGrkoGwuxK8=\"}"
---
+++ .next/static/chunks/app/product/chuot/page-79f578ab48b7dbda.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[996],{},function(n){n.O(0,[749,795,250,183,734,971,69,744],function(){return n(n.s=4734)}),_N_E=n.O()}]);
